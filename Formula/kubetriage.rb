eba00cc85344874550ef28449542f7b77fd3e8a9
Avishay Ashkenazi <52656881+aavishay@users.noreply.github.com>
2026-04-21 11:47:16 +0300

    Update kubetriage to v0.0.3

--- Formula/kubetriage.rb
+++ Formula/kubetriage.rb
@@ -2,27 +2,27 @@ class Kubetriage < Formula
   desc "KubeTriage - Autonomous SRE Guard for Kubernetes"
   homepage "https://github.com/aavishay/kubetriage"
   license "MIT"
-  version "0.0.1"
+  version "0.0.3"
 
   on_macos do
     on_arm do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-darwin-arm64.tar.gz"
-      sha256 "b9c3b00bb62a9105d5398da6b5bd5625cda75a080c5a3012c4677bf095731e12"
+      sha256 "a2803b8f595e051757c917bcad7ece231e3b4666a6f0b5a243d8170d9884e587"
     end
     on_intel do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-darwin-amd64.tar.gz"
-      sha256 "a1247558a6c43c22a7a7a4c4cc7d205d658046be4ea91f2763e763bda07a5ae5"
+      sha256 "343104c7d84582f3050649af5c5364fb470eb3f03e07a736758f9066e773d7d2"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-linux-amd64.tar.gz"
-      sha256 "04892aa86984b70e044c35442aadcf5b7c988f34dfa6b765fb1efffc96eab806"
+      sha256 "6d816fbe3574d7bc009e5d6602b56b3f50d96434c1b15a60db65d59c24e76f07"
     end
     on_arm do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-linux-arm64.tar.gz"
-      sha256 "802f5141994ef96a67e4cc92230b0e6fc536e18fa1d080ccf5d5c72426722cf1"
+      sha256 "e593127337a08a83606d8ef9caf15cc89d02842903b49bb3cf52c3775e979e4d"
     end
   end
 
