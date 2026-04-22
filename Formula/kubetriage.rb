1e7a75656f8d8fbbedd2b104351bf44c76f0bcde
Avishay Ashkenazi <aavishay@gmail.com>
2026-04-22 16:28:56 +0300

    bump kubetriage to 1.4.0

--- Formula/kubetriage.rb
+++ Formula/kubetriage.rb
@@ -1,33 +1,35 @@
 class Kubetriage < Formula
-  desc "KubeTriage - Autonomous SRE Guard for Kubernetes"
+  desc "Autonomous SRE Guard for Kubernetes"
   homepage "https://github.com/aavishay/kubetriage"
+  version "1.4.0"
   license "MIT"
-  version "1.2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-#{version}-darwin-arm64.tar.gz"
-      sha256 "48a10631b8e966a15f74584f8f81893941347befb4bf2cc8e2b3f91d11569f44"
+      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-darwin-arm64.tar.gz"
+      sha256 "312b3989f37ff1d290b04ff8c7b22e34fd26b4f3ee79deaf04b96e78832ec044"
     end
     on_intel do
-      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-#{version}-darwin-amd64.tar.gz"
-      sha256 "72619ea52943128b8ba72ce5e13f8474b471853ebbeafc2087b42df82242ed47"
+      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-darwin-amd64.tar.gz"
+      sha256 "0b497210f742b155d52efb0390511cf1005c6c85ebff1801100977e63d359cfa"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-#{version}-linux-amd64.tar.gz"
-      sha256 "c87dfeb39c5a08a1c00187affc66fbe50a786ae37021f3dcf7da1a3b8d904c47"
+      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-linux-amd64.tar.gz"
+      sha256 "a688f644a7cc989f3aac979cb37803b25079fec33f7002d4aeb874da43397540"
     end
     on_arm do
-      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-#{version}-linux-arm64.tar.gz"
-      sha256 "3949aace6962b1f5a8bb6410d8bd27b23b96992afccf8dd76c23c27bcc02539e"
+      url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-linux-arm64.tar.gz"
+      sha256 "b1a6d2867398597645612b9584d0848d10049d1b32d47ad4d059e38c251b0afd"
     end
   end
 
   def install
-    bin.install "kubetriage-#{OS.kernel_name.downcase}-#{Hardware::CPU.arch.to_s.gsub(/x86_64/, "amd64")}" => "kubetriage"
+    binary_name = "kubetriage-#{OS.kernel_name.downcase}-#{Hardware::CPU.arch}"
+    binary_name = binary_name.gsub("x86_64", "amd64")
+    bin.install binary_name => "kubetriage"
   end
 
   def post_install
@@ -37,6 +39,6 @@ class Kubetriage < Formula
   end
 
   test do
-    system "#{bin}/kubetriage", "--help"
+    system bin/"kubetriage", "--help"
   end
 end
